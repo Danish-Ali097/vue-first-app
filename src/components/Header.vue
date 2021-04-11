cc943c445d215af0ca97a13fa1ef5728646c5f1a
Danish Ali <danish@codendevs.com>
2021-04-11 12:04:00 +0500

    added router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,12 @@
 <template>
     <header>
         <h1>{{title}}</h1>
-        <Button @btn-click="$emit('toggle-add-task')"
+        <Button 
+        v-show="homePage"
+        @btn-click="$emit('toggle-add-task')"
         :text="showAddTask ? 'Close' : 'Add Task'"
-        :color="showAddTask ? 'red' : 'green'" />
+        :color="showAddTask ? 'red' : 'green'"
+        />
     </header>
 </template>
 
@@ -17,6 +20,15 @@ export default {
     },
     components:{
         Button
+    },
+    computed: {
+        homePage(){
+            if(this.$route.path === '/') {
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
 </script>
